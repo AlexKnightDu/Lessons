72460c8c4b8482fc9f4ec2a83d42d900e03c95ee
AlexKnightDu <dulianyu@sjtu.edu.cn>
2018-05-31 10:55:46 +0800

    run

--- ML/HW3/hw3_1.py
+++ ML/HW3/hw3_1.py
@@ -7,8 +7,8 @@ import numpy as np
 import os
 import time
 
-train_data_file = './a5a.txt'
-test_data_file = './a5a.t'
+train_data_file = './a9a.txt'
+test_data_file = './a9a.t'
 
 kernel_type = ['linear', 'poly', 'rbf', 'sigmoid']
 
@@ -17,7 +17,7 @@ feature_num = 123
 def train(kernel):
 
     time_stamp = time.strftime("%H-%M-%S",time.localtime())
-    fout = open('./result/' + kernel_type[kernel] + '_' + time_stamp + '.out', 'w+')
+    fout = open('./result/M' + kernel_type[kernel] + '_' + time_stamp + '.out', 'w+')
     print('kernel ' + kernel_type[kernel] + ' started ' + '*' * 20)
     print('the process parent id :',os.getppid())
     print('the process id is :',os.getpid())
@@ -30,16 +30,16 @@ def train(kernel):
     g_para = list(np.logspace(0,6,7,base=2)/(8 * feature_num))
     d_para = list(range(2,7))
     r_para = list(range(0,3,1))
-    for r in r_para:
+    for c in c_para:
+        c = round(c, 5)
         for g in g_para:
             g = round(g, 5)
-            for c in c_para:
-                c = round(c, 5)
+            for r in r_para:
                 if (str(kernel) == '1'):
                     for d in d_para:
                         param = '-t ' + str(kernel) + ' -c ' + str(c) + ' -g ' + str(g) + ' -r ' + str(r) + ' -d ' + str(d)
                         fout.write(param + ' ')
-                        param = svm_parameter(param + ' -b 1 -m 5000 -q')
+                        param = svm_parameter(param + ' -b 1 -m 10000 -h 1 -q')
                         problem = svm_problem(y,x)
                         model = svm_train(problem, param)
                         p_label, p_acc, p_val = svm_predict(yt, xt, model)
@@ -48,7 +48,7 @@ def train(kernel):
                 else:
                     param = '-t ' + str(kernel) + ' -c ' + str(c) + ' -g ' + str(g) + ' -r ' + str(r)
                     fout.write(param + ' ')
-                    param = svm_parameter(param + ' -b 1 -m 5000 -q')
+                    param = svm_parameter(param + ' -b 1 -m 10000 -h 1 -q')
                     problem = svm_problem(y, x)
                     model = svm_train(problem, param)
                     p_label, p_acc, p_val = svm_predict(yt, xt, model)
